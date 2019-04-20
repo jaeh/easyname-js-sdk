6fcf8bb16a90842375265e5f4e56c53a97f33aed
Jascha <jascha@jaeh.at>
2019-04-20 12:33:56 +0200

    add meta files

---
+++ README.md
@@ -0,0 +1,8 @@
+easyname - JS SDK
+==================
+
+Use this SDK to interact easily with our REST API.
+
+Requirements
+------------
+nodejs >= 10.0.0
---
+++ config/sample.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  url: 'https://api.easyname.com',
+  user: {
+    id: 0,
+    email: 'foo@example.org',
+  },
+  api: {
+    key: 'aaaaaaa',
+    authenticationSalt: 'aaaaaaaa%sbbbbbbbb%scccccccc',
+    signingSalt: 'AaaBbbCccDddEeeFffGggHhh',
+  },
+  debug: true,
+}
